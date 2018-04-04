33af796500aca689622c18d4f526f99e75d18c40
hakimix <star1994@hotmail.dk>
2018-04-04 21:24:52 +0200

    edit

--- routes/index.js
+++ routes/index.js
@@ -19,96 +19,96 @@ var wit_token = 'D7FKRWLJRNYUKEACKGENJQG7EOLISSMJ';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Express'
-  });
+    res.render('index', {
+        title: 'Express'
+    });
 });
 
 router.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === 'verify_me') {
-    res.send(req.query['hub.challenge']);
-  }
-  res.send('Error, wrong validation token');
+    if (req.query['hub.verify_token'] === 'verify_me') {
+        res.send(req.query['hub.challenge']);
+    }
+    res.send('Error, wrong validation token');
 });
 
 router.post('/webhook/', function (req, res) {
-  messaging_events = req.body.entry[0].messaging;
+    messaging_events = req.body.entry[0].messaging;
 
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
 
-    if (event.message && event.message.text) {
-      text = event.message.text;
+        if (event.message && event.message.text) {
+            text = event.message.text;
 
-      switch (text) {
-        case "artikler":
-          api.getArticles();
-          timers.setTimeout(() => platform.sendArticleMessage(sender), 2000);
-          break;
-        case "generic":
-          platform.generic(sender);
-          break;
-        case "stop":
-          platform.sendText(sender, "jeg stopper");
-          break;
-        default:
-          callWithAI(text, function (err, intent) {
-            handleIntent(intent, sender);
-          })
-      }
+            switch (text) {
+                case "artikler":
+                    api.getArticles();
+                    timers.setTimeout(() => platform.sendArticleMessage(sender), 2000);
+                    break;
+                case "generic":
+                    platform.generic(sender);
+                    break;
+                case "stop":
+                    platform.sendText(sender, "jeg stopper");
+                    break;
+                default:
+                    callWithAI(text, function (err, intent) {
+                        handleIntent(intent, sender);
+                    })
+            }
 
-      //sendText(sender, text);
+            //sendText(sender, text);
+        }
+        if (event.postback) {
+            let text = JSON.stringify(event.postback)
+        }
     }
-    if (event.postback) {
-      let text = JSON.stringify(event.postback)
-    }
-  }
-  res.sendStatus(200);
+    res.sendStatus(200);
 });
 
 
 // WIT AI Connection
 function callWithAI(query, callback) {
-  query = encodeURIComponent(query);
-  request({
-    uri: wit_endpoint + query,
-    qs: {
-      access_token: wit_token
-    },
-    method: 'GET'
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log('Successfully got %s', response.body);
-      try {
-        body = JSON.parse(response.body)
-        intent = body['entities']['intent'][0]['value']
-        callback(null, intent)
-      } catch (e) {
-        callback(e)
-      }
-    } else {
-      console.log(response.statusCode)
-      console.error("Unable to send message. %s", error);
-      callback(error)
-    }
-  });
+    query = encodeURIComponent(query);
+    request({
+        uri: wit_endpoint + query,
+        qs: {
+            access_token: wit_token
+        },
+        method: 'GET'
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('Successfully got %s', response.body);
+            try {
+                body = JSON.parse(response.body)
+                intent = body['entities']['intent'][0]['value']
+                callback(null, intent)
+            } catch (e) {
+                callback(e)
+            }
+        } else {
+            console.log(response.statusCode)
+            console.error("Unable to send message. %s", error);
+            callback(error)
+        }
+    });
 }
 
 
 // WIT AI Intents
 function handleIntent(intent, sender) {
-  switch (intent) {
-    case "greeting":
-      platform.sendText(sender, "Hi! how can i help you?");
-      break;
-    case "help":
-      platform.sendText(sener, "I am here to help!");
-      break;
-    default:
-      platform.sendText(sender, "I don't understand :(");
-      break;
-  }
+    switch (intent) {
+        case "greeting":
+            platform.sendText(sender, "Hi! how can i help you?");
+            break;
+        case "help":
+            platform.sendText(sener, "I am here to help!");
+            break;
+        default:
+            platform.sendText(sender, "I don't understand :(");
+            break;
+    }
 }
 
 
